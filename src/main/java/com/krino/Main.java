e990d242092af63e0de63f8919da044555d4b003
krino45 <krina645@gmail.com>
2025-02-18 19:51:20 +0700

    Final version, add an "average" thread (whole progress's progresses bar)

--- src/main/java/com/krino/Main.java
+++ src/main/java/com/krino/Main.java
@@ -5,12 +5,10 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Main {
-    static int symbol_count = 120;
-    static CyclicBarrier cyclicBarrier;
+    static int symbol_count = 40;
     static CountDownLatch cdl;
-    static String os = System.getProperty("os.name");
     static final int thread_count = 10;
-    static final Duration duration = Duration.ofSeconds(2);
+    static final Duration loading_duration = Duration.ofSeconds(2);
 
     public static class Tuple<X, Y> {
         public final X x;
@@ -21,32 +19,34 @@ public class Main {
         }
     }
 
-    static HashMap<String,  Tuple<Integer, Long>> progressReport = new HashMap<>();
+    static Map<String,  Tuple<Integer, Long>> progressReport = new ConcurrentHashMap<>();
 
     public synchronized static void ProgressWriter(String name, int currentCount, long startingTime){
-        progressReport.put(name, new Tuple<Integer, Long>(currentCount, startingTime));
-        try {
-            if (os.contains("Windows")) {
-                System.out.print("\033["+(thread_count + 3)+"A");
-            }
-            else
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("\n=========");
+        progressReport.put(name, new Tuple<>(currentCount, startingTime));
+        int uplines = progressReport.size() + 3; // 1 accounts for the ======== line
         ArrayList<Map.Entry<String, Tuple<Integer, Long>>> entries = new ArrayList<>(progressReport.entrySet());
         entries.sort(Comparator.comparingInt(entry -> Integer.parseInt(entry.getKey().replaceAll("\\D+", ""))));
+        System.out.print("\033["+uplines+"A");
+        System.out.println(((cdl.getCount() == 0) ? "All" : thread_count - cdl.getCount())+ " threads finished");
         for (Map.Entry<String, Tuple<Integer, Long>> entry : entries) {
             Duration duration = Duration.ofNanos(System.nanoTime() - entry.getValue().y);
             System.out.println(entry.getKey() +
                     " ".repeat(16 - entry.getKey().length()) +
-                    "[" + "#".repeat(entry.getValue().x) +
-                    " ".repeat(symbol_count - entry.getValue().x - 1) + "]" + (((entry.getValue().x + 1) >= symbol_count) ? "" :
+                    "[" + "\033[32m" + "#".repeat(entry.getValue().x) + "\033[39m" +
+                    "~".repeat(symbol_count - entry.getValue().x - 1) + "]" + (((entry.getValue().x + 1) >= symbol_count) ? "" :
                     " " + duration.toSeconds() +"."+ duration.toMillis() + "s")
                     );
         }
-        System.out.print("\033["+(thread_count + 3)+"B");
+        int average = progressReport.values().stream().map(t -> t.x).reduce(0,Integer::sum) / thread_count;
+        long max = progressReport.values().stream().map(t -> t.y).max(Long::compare).orElseThrow();
+        Duration duration = Duration.ofNanos(System.nanoTime() - max);
+        System.out.println();
+        System.out.println("Main" +
+                " ".repeat(12) +
+                "[" + "\033[32m" + "#".repeat(average) + "\033[39m" +
+                "~".repeat(symbol_count - average - 1) + "]" + (((average) >= symbol_count) ? "" :
+                " " + duration.toSeconds() +"."+ duration.toMillis() + "s")
+        );
     }
 
     static class MyThread implements Runnable {
@@ -65,18 +65,13 @@ public class Main {
             long startTime = System.nanoTime();
             for(int currentSymbols = 0; currentSymbols < symbol_count; currentSymbols++) {
                 try {
-                    Thread.sleep(millisPerSymbol + (((new Random()).nextBoolean()) ? -1L : 1L) * (new Random()).nextLong(millisPerSymbol / 2));
+                    Thread.sleep(millisPerSymbol + (((new Random()).nextBoolean()) ? -1L : 1L) * (new Random()).nextLong(millisPerSymbol / 2,millisPerSymbol));
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 ProgressWriter(name, currentSymbols, startTime);
             }
-            try {
-                cyclicBarrier.await();
-                cdl.countDown();
-            } catch (InterruptedException | BrokenBarrierException e) {
-                throw new RuntimeException(e);
-            }
+            cdl.countDown();
         }
     }
 
@@ -84,11 +79,10 @@ public class Main {
         System.out.println("Starting execution!" + "\n".repeat(thread_count + 2));
         Thread.sleep(1000);
         ExecutorService executorService;
-        cyclicBarrier = new CyclicBarrier(thread_count, null);
-        cdl = new CountDownLatch(1);
+        cdl = new CountDownLatch(thread_count);
         executorService = Executors.newFixedThreadPool(thread_count);
         for (int i = 1; i <= thread_count; i++) {
-            executorService.execute(new MyThread("Thread" + i, duration));
+            executorService.execute(new MyThread("Thread" + i, loading_duration));
         }
         cdl.await();
         executorService.shutdown();
