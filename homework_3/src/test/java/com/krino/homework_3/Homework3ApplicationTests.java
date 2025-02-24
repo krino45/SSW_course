7997a20b1f5e547bac8f20ad6a04c742c5ac5ed9
krino45 <krina645@gmail.com>
2025-02-24 15:04:16 +0700

    Finish homework 3

---
+++ homework_3/src/test/java/com/krino/homework_3/Homework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krino.homework_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
